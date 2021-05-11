8f90a78be21fa3de6f0aafbe85181a5119c25140
frankmorara <franklinenyabuto11@gmail.com>
2021-05-11 10:17:47 +0300

    Creating a click listener to the playButton and navigating to the game Fragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment: Fragment(R.layout.fragment_title) {
@@ -17,6 +19,10 @@ class TitleFragment: Fragment(R.layout.fragment_title) {
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
             inflater, R.layout.fragment_title, container, false
         )
+        binding.playButton.setOnClickListener {
+//            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+            view?.findNavController()?.navigate(R.id.action_titleFragment_to_gameFragment)
+        }
         return  binding.root
     }
 
